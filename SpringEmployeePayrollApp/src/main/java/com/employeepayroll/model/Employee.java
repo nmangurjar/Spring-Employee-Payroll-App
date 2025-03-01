ad56990ceb31714547b703d9a0ec1668d4b9010e
nmangurjar <131614724+nmangurjar@users.noreply.github.com>
2025-03-01 12:05:34 +0530

    [Naman] Add. Ability to develop rest of the CRUD Service Methods with MySQL Database

--- SpringEmployeePayrollApp/src/main/java/com/employeepayroll/model/Employee.java
+++ SpringEmployeePayrollApp/src/main/java/com/employeepayroll/model/Employee.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
